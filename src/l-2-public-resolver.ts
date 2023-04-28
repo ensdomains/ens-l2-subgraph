5275946720dacf0487823fcb9c324f11e32ceb70
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-04-28 17:49:48 +0100

    Add Domain and Resolver

--- src/l-2-public-resolver.ts
+++ src/l-2-public-resolver.ts
@@ -6,17 +6,35 @@ import {
 import {
   AddrChanged,
   AddressChanged,
-  TextChanged
+  TextChanged,
+  Resolver,
+  Domain
 } from "../generated/schema"
-import { log } from '@graphprotocol/graph-ts'
+import { Address, Bytes, log } from '@graphprotocol/graph-ts'
 
 export function handleAddrChanged(event: AddrChangedEvent): void {
   log.warning('*******handleAddrChanged', [])
+  let node = event.params.node.toHexString()
   let entity = new AddrChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
+
+  let resolver = new Resolver(
+    createResolverID(event.params.node, event.address)
+  );
+  resolver.address = event.address;
+  resolver.ownedNode = event.params.ownedNode.toHexString();
+  resolver.addr = event.params.a;
+  resolver.owner = event.transaction.from;
+  resolver.save();
+
+  let  domain = new Domain(node);
+  domain.resolvedAddress = event.params.a;
+  domain.resolver = resolver.id;
+  domain.save()
+
   entity.node = event.params.node
-  entity.ownedNode = event.params.ownedNode
+  // entity.ownedNode = event.params.ownedNode.toHexString();
   entity.a = event.params.a
 
   entity.blockNumber = event.block.number
@@ -58,4 +76,11 @@ export function handleTextChanged(event: TextChangedEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+}
+
+function createResolverID(node: Bytes, resolver: Address): string {
+  return resolver
+    .toHexString()
+    .concat("-")
+    .concat(node.toHexString());
 }
