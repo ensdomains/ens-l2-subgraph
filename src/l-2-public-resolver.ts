e7478baf33c14c3fb4c5d2cb228c14260afe85d3
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-08-08 11:28:46 +0100

    Remove logger

--- src/l-2-public-resolver.ts
+++ src/l-2-public-resolver.ts
@@ -2,7 +2,6 @@ import {
   AddrChanged as AddrChangedEvent,
   AddressChanged as AddressChangedEvent,
   TextChanged as TextChangedEvent,
-  // NameSet as NameSetEvent
 } from "../generated/L2PublicResolver/L2PublicResolver"
 
 import {
@@ -27,7 +26,6 @@ export function handleName(node:Bytes, context:Bytes, dnsName:Bytes): void {
   if(!domain){
     domain = new Domain(domainId)
   }
-  log.warning("*** handleName1 {}", [domainId])
   let decoded = decodeName(dnsName)
   if(decoded){    
     let labelName = decoded[0]
@@ -36,7 +34,6 @@ export function handleName(node:Bytes, context:Bytes, dnsName:Bytes): void {
     let name = decoded ? decoded[1] : ''
     let parentEncoded = decoded ? decoded[3] : ''
     let parentNode = namehash(Bytes.fromHexString(parentEncoded))
-    log.warning("*** handleName2 {}", [name])
     domain.name = name
     domain.labelName = labelName
     domain.labelhash = Bytes.fromHexString(labelhash)
@@ -64,31 +61,25 @@ export function handleName(node:Bytes, context:Bytes, dnsName:Bytes): void {
 }
 
 export function handleAddrChanged(event: AddrChangedEvent): void {
-  log.warning("*** handleAddrChanged1", [])
   let entity = new AddrChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  log.warning("*** handleAddrChanged2, {}", [event.params.node.toHexString()])
   let resolver = getOrCreateResolver(
     event.params.node,
     event.params.context,
     event.address,
   )
-  log.warning("*** handleAddrChanged3", [])
   resolver.addr = event.params.a;
   resolver.domain = createDomainID(event.params.node, event.params.context)
   resolver.save();
-  log.warning("*** handleAddrChanged4", [])
   let domain = createDomain(
     event.params.node,
     event.params.context,
     resolver.id
   )
-  log.warning("*** handleAddrChanged5", [])
   domain.resolvedAddress = event.params.a;
   domain.save()
   handleName(event.params.node, event.params.context, event.params.name)
-  log.warning("*** handleAddrChanged5.1", [])
   entity.context = event.params.context
   entity.name = event.params.name
   entity.node = event.params.node
@@ -97,13 +88,10 @@ export function handleAddrChanged(event: AddrChangedEvent): void {
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
-  log.warning("*** handleAddrChanged6", [])
   entity.save()
-  log.warning("*** handleAddrChanged7", [])
 }
 
 export function handleAddressChanged(event: AddressChangedEvent): void {
-  log.warning("*** handleAddressChanged", [])
   let entity = new AddressChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
@@ -144,8 +132,6 @@ export function handleAddressChanged(event: AddressChangedEvent): void {
 }
 
 export function handleTextChanged(event: TextChangedEvent): void {
-  log.warning("*** handleTextChanged1", [])
-  let node = event.params.node.toHexString();
   let entity = new TextChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
@@ -168,11 +154,9 @@ export function handleTextChanged(event: TextChangedEvent): void {
   )
   let key = event.params.key;
   if(resolver.texts == null) {
-    log.warning("*** handleTextChanged1", [])
     resolver.texts = [key];
     resolver.save();
   } else {
-    log.warning("*** handleTextChanged2", [])
     let texts = resolver.texts!
     if(!texts.includes(key)){
       texts.push(key)
